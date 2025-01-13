1ca10ac9d6104cbb6ed460d97cb278430c626c30
praveen kanth <praveenusain111@gmail.com>
2025-01-13 17:50:24 +0530

    deployment-fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  env: {
-    MONGO_DB_URL: process.env.MONGO_URL,
-  },
 };
 
 export default nextConfig;

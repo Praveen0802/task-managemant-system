ba155063e4afc3150fb086984893d5e28b68552d
praveen kanth <praveenusain111@gmail.com>
2025-01-13 18:11:46 +0530

    env-files-added

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_MONGO_URL:
+      "mongodb+srv://praveenusain111:iZRoXoOoMc77fHcE@cluster-1.p7hcr.mongodb.net/task_management?retryWrites=true&w=majority",
+  },
 };
 
 export default nextConfig;
